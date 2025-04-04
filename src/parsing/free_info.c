aad4ecf3a2eb8bbba27bacb3fa5f259eefa3569f
Ariel AMRIOU <ariel.amriou@epitech.eu>
2025-04-04 17:38:56 +0200

    fix: remove of leaks

--- src/parsing/free_info.c
+++ src/parsing/free_info.c
@@ -26,11 +26,28 @@ void free_room(void *data)
     free(room);
 }
 
+static void free_rooms_list(rooms_t *rooms_list, int const len)
+{
+    if (rooms_list == NULL)
+        return;
+    for (int i = 0; i < len; i++) {
+        free(rooms_list[i].links);
+        free(rooms_list[i].name);
+    }
+    free(rooms_list);
+}
+
 void free_info(info_t *info)
 {
+    int len = 0;
+
     if (info == NULL)
         return;
+    len = info->rooms->len;
     free_matirce(info->matrice, info->rooms->len);
+    free_rooms_list(info->rooms_list, len);
     destroy_list(info->rooms, free_room);
+    free(info->name_start);
+    free(info->name_end);
     free(info);
 }
